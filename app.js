da230cc5272a8125e870a4e5c7b8cef45e143c3e
Brandon Johnson <bmj1985@gmail.com>
2018-06-07 18:04:43 -0600

    boilerplate pusher code

--- app.js
+++ app.js
@@ -5,12 +5,25 @@ const bodyParser = require('body-parser')
 const runners = require('./routes/runners')
 const twilio = require('./routes/twilio')
 const morgan = require('morgan')
-const devMode = process.env.NODE_ENV !== 'production';
+const Pusher = require('pusher')
+const devMode = process.env.NODE_ENV !== 'production'
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(morgan(devMode ? 'dev' : 'combined'))
 app.use(cors())
+
+const pusher = new Pusher({
+  appId: '538240',
+  key: '0c52d4e3dcef6077075b',
+  secret: 'f7cfbac19823d6b9b3ca',
+  cluster: 'us2',
+  encrypted: true
+})
+
+pusher.trigger('my-channel', 'my-event', {
+  'message': 'hello world'
+})
 
 app.get('/api/beta', (req, res) => {
   res.json({
